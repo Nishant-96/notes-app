bae2d0ab4a06f23bf7ac96ad6a818a856cba6b50
Nishant-96 <nishant11pratihast@gmail.com>
2022-05-01 00:39:40 +0530

    feat: route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import Mockman from "mockman-js";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import { Navbar, SideNav } from "./components";
+import {
+  Archive,
+  Home,
+  Label,
+  Login,
+  Notes,
+  PageNotFound,
+  SignUp,
+  Trash,
+} from "./pages";
+import { Forgot } from "./pages/auth/forgot";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      {/* <Notes /> */}
+      {/* <Trash /> */}
+      {/* <Label /> */}
+      {/* <Archive /> */}
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      {/* <Forgot /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/label" element={<Label />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/trash" element={<Trash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<Forgot />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/mockman" element={<Mockman />} />
+      </Routes>
     </div>
   );
 }
