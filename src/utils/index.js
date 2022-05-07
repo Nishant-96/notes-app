f38a8817ad1ec502db45d3a0d707a04a9cdb14ba
Nishant-96 <nishant11pratihast@gmail.com>
2022-05-07 21:18:55 +0530

    feat: delete, post archive note services

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,5 @@
 export { postNotesHandler } from "./services";
 export { postNoteEditHandler } from "./services";
+export { postArchiveNoteHandler } from "./services";
+export { postRestoreArchiveHandler } from "./services";
+export { deleteArchiveNoteHandler } from "./services";
--- src/utils/services.js
+++ src/utils/services.js
@@ -39,3 +39,59 @@ export const postNoteEditHandler = async (noteId, note, token, dispatch) => {
     console.error(error);
   }
 };
+
+export const postArchiveNoteHandler = async (noteId, note, token, dispatch) => {
+  try {
+    const {
+      data: { archives, notes },
+    } = await axios.post(
+      `/api/notes/archives/${noteId}`,
+      {
+        note,
+      },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    dispatch({ type: "ARCHIVE_NOTE", payload: { notes, archives } });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postRestoreArchiveHandler = async (
+  noteId,
+  note,
+  token,
+  dispatch
+) => {
+  try {
+    const {
+      data: { archives, notes },
+    } = await axios.post(
+      `/api/archives/restore/${noteId}`,
+      { note },
+      {
+        headers: { authorization: token },
+      }
+    );
+    dispatch({ type: "ARCHIVE_NOTE", payload: { notes, archives } });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteArchiveNoteHandler = async (noteId, token, dispatch) => {
+  try {
+    const {
+      data: { archives },
+    } = await axios.delete(`/api/archives/delete/${noteId}`, {
+      headers: { authorization: token },
+    });
+    dispatch({ type: "DELETE_ARCHIVE_NOTE", payload: { archives } });
+  } catch (error) {
+    console.error(error);
+  }
+};
